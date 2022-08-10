6ba9fd73dc411f3ad49e5520d2c6ad24ca1356ee
Noah King <noahking123@yahoo.com>
2022-08-10 14:02:12 -0400

    commit tree 00640ea9de0db67bb0bf18071afbae2503201d50

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+//const myArray = ['Guadalupe','Ollie','Aki', 'Suprise'];
+
+function writeCards(names, event) {
+    console.log(names);
+    const cards = []
+    for (let i = 0; i < names.length; i++)  {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    console.log(cards);
+    return cards;
+}
+
+function countDown(count) {
+    let number = count;
+    while (number >= 0) {
+        console.log(number--);
+    }
+    return number;
+}
